8ee2871a8f8d5a57082ab9b07eb8eb939d97af33
Matheus <matheusdesaaguiar@gmail.com>
2026-04-25 17:42:06 -0300

    feat: Busca implementada

--- trabalho-aoc1/main.c
+++ trabalho-aoc1/main.c
@@ -7,48 +7,46 @@ unsigned short int mar, imm, pc = 0, reg[8];
 unsigned char ir, ro0, ro1;
 
 void search(){
-    //memory[0] = 0b01110;
-    memory[0]= 0b01110;
-    memory[1]= 0b;
+    memory[0] = 0b00100000;
+    memory[1] = 0b01100000;
+    memory[2] = 0b00011110;
+
     mar = pc;
     // 0000 0000 0000 0000 0000 0000 0000 0000
     mbr = memory[mar];
     // 0000 0000 0000 0000 0000 0000 0000 0000
-    printf("%024b\n", mbr);
 
-    ir = mbr;
-    printf("%i\n", ir);
+    ir = mbr >> 3;
 
     // hlt nop not
     if(ir == 0 || ir == 1 || ir == 13) {
         mbr = mbr << 8;
         mbr = mbr << 8;
     } else if(ir >=2 && ir <=12) {
-        mbr = mbr << 8;
         mar ++;
+        mbr = mbr << 8;
         mbr = mbr | memory[mar];
         mbr = mbr <<8;
-        mbr = mbr | memory[mar];
-        mbr = mbr <<3;
     } else if(ir >= 14 && ir <=20){
-        mbr = mbr << 11;
         mar++;
+        mbr = mbr << 8;
         mbr = mbr | memory[mar];
+        mar++;
+        mbr = mbr << 8;
+        mbr = mbr | memory[mar];
+    } else if(ir >=21 && ir<=29) {
+        mbr = mbr << 8;
         mar++;
         mbr = mbr | memory[mar];
         mbr = mbr << 8;
-    } else if(ir >=21 && ir<=29) {
-
+        mar++;
+        mbr = mbr | memory[mar];
     }
-    printf("%024b\n",mbr);
-
 }
 
 int main()
 {
     search();
-
-    // para o commit
     return 0;
 }
 
